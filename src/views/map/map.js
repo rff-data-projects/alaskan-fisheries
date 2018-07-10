61c13079ec6789fd70861d8fc958e8ef91f47246
John Osterman <ostermanj@gmail.com>
2018-07-10 15:16:11 -0400

    Statewide fisheries are in

--- src/views/map/map.js
+++ src/views/map/map.js
@@ -1,13 +1,18 @@
 import s from './styles.scss';
 import map from './map.html';
+import legend from './legend.html';
 export default {
     init(){
        console.log(s);
         var div = document.createElement('div');
         div.className = 'map-container';
         div.innerHTML = map;
-
         document.querySelector('.main-column').appendChild(div);
+
+        var legendDiv = document.createElement('div');
+        legendDiv.className = 'legend-container';
+        legendDiv.innerHTML = legend;
+        document.querySelector('.main-column').appendChild(legendDiv);
 
         return div;
     }
